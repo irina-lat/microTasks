6a5f23dd6efdb56d9a08a1d1b3c29ecd35795e74
irina-lat <irochka.bars@mail.ru>
2022-07-01 21:36:06 +0300

    add InputFull.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import './App.css';
 import {Header} from "./Components/Header";
 import {Body} from "./Components/Body";
 import {Footer} from "./Components/Footer";
+import {InputFull} from "./Components/InputFull";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Body/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Body/>
+            <Footer/>
+            <InputFull/>
+
+        </div>
+    );
 }
 
 export default App;
